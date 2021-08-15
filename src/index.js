35fea644241c7914729ffa1018c736a1076bfb8b
Svetlana <sveta-dekt@tut.by>
2021-08-15 17:01:07 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+  if(array == undefined || array.length == 0) {
+    return 0;
+  }
+  if (array.length > 1) {
+     return Math.min.apply(null, array);
+  }
+};
+
 
 exports.max = function max (array) {
-  return 0;
-}
+  if(array == undefined || array.length == 0) {
+    return 0;
+  }
+  if (array.length > 1) {
+     return Math.max.apply(null, array);
+  }
+};
+
 
 exports.avg = function avg (array) {
-  return 0;
-}
+  let avgValue = 0;
+  if(array == undefined || array.length == 0) {
+    return 0;
+  }
+  if (array.length > 1) {
+    let sum = array.reduce(function(a,b) {
+      return a + b;
+    });
+     avgValue = sum / array.length;
+  }
+  
+  return avgValue;
+};
+
